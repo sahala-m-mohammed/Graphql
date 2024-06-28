7c750a2a9136c5f92797b0b10ed917eb5020fd77
Sahala M Mohammed <sahalammohammed07@gmail.com>
2024-06-28 13:43:48 +0530

    added graphql files

---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/sahala/hackernews/graph"
+	"github.com/sahala/hackernews/internal/auth"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+	database "github.com/sahala/hackernews/internal/pkg/db/migrations/mysql"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(auth.Middleware())
+
+	database.InitDB()
+	database.Migrate()
+	server := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", server)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
